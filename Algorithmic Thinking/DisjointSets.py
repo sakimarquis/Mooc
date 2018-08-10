6aca97e07a27f6fd23f25cc2f7e3987a99504f73
SakiMarquis <sakimarquis@qq.com>
2018-08-10 19:36:01 +0800

    Add files via upload

--- Algorithmic Thinking/DisjointSets.py
+++ Algorithmic Thinking/DisjointSets.py
@@ -30,13 +30,15 @@ Created on Thu Aug  9 09:50:53 2018
 @author: Saki
 """
 
-class DisjointSets:
+class DisjointSets1:
     def __init__(self, nodes):
         self._parents = {}
-        self._ranks = {}     
+        self._ranks = {}
+        self._sizes = {}
         for node in range(nodes):
             self._parents[node] = node
-            self._ranks[node] = 1
+            self._ranks[node] = 0
+            self._sizes[node] = 1
     
     def find(self, node):
         if node != self._parents[node]:
@@ -45,41 +47,45 @@ class DisjointSets:
         return self._parents[node]
     
     def union(self, x_set, y_set):
-        x_root = self.find(x_set) 
-        y_root = self.find(y_set)
-        if self._ranks[x_root] > self._ranks[y_root]:
-            self._parents[y_root] = x_root
-        elif self._ranks[x_root] < self._ranks[y_root]:
-            self._parents[x_root] = y_root
-        else:        
-            self._parents[y_root] = x_root
-            self._ranks[x_root] += 1
-    
-    def read_graph(self, ugraph):
-        self._parents = {}
-        self._ranks = {}
-        for parent, childs in ugraph.items():
-            if len(childs) == 0:
-                self._parents[parent] = parent                 
-            else:
-                for child in childs:
-                    if self._parents.get(child) == None:
-                        self._parents[child] = parent                        
-   
-    def get_parents(self):
-        return self._parents
-    
-    def get_ranks(self):
-        return self._ranks
-            
+        x_root, y_root = self.find(x_set), self.find(y_set)
+        if x_root != y_root:
+            if self._ranks[x_root] > self._ranks[y_root]:
+                self._parents[y_root] = x_root
+                self._sizes[x_root] = self._sizes[x_root] + self._sizes[y_root]
+            elif self._ranks[x_root] < self._ranks[y_root]:
+                self._parents[x_root] = y_root
+                self._sizes[y_root] = self._sizes[x_root] + self._sizes[y_root]
+            else:        
+                self._parents[y_root] = x_root
+                self._sizes[x_root] = self._sizes[x_root] + self._sizes[y_root]
+                self._ranks[x_root] += 1
+
+                
+#    def union(self, x_set, y_set):
+#        x_root, y_root = self.find(x_set), self.find(y_set)
+#        if x_root != y_root:
+#            if self._sizes[x_root] >= self._sizes[y_root]:
+#                self._parents[y_root] = x_root
+#                self._sizes[x_root] = self._sizes[x_root] + self._sizes[y_root]
+#            else:
+#                self._parents[x_root] = y_root
+#                self._sizes[y_root] = self._sizes[x_root] + self._sizes[y_root]
+
         
-a = DisjointSets(10)
+a = DisjointSets1(10)
+a.union(1,3)
+a.union(3,4)
+a.union(8,9)
+print(a._sizes)
 
 # test
 ugraph = {0:set([1,4,5]),
-         1:set([0,2,6]),
-         2:set([1,3]),
-         3:set([2]),
-         4:set([0]),
-         5:set([0]),
-         6:set([1])}
+          1:set([0,2,6]),
+          2:set([1,3]),
+          3:set([2]),
+          4:set([0]),
+          5:set([0,6]),
+          6:set([1,5]),
+          7:set([]),
+          8:set([9]),
+          9:set([8])}
