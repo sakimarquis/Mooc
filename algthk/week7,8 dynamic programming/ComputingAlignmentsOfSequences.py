3208cffef99a501252512e39083dd7038efbff13
sakimarquis <sakimarquis@qq.com>
2018-08-20 15:37:15 +0800

    finish project4

--- algthk/week7,8 dynamic programming/ComputingAlignmentsOfSequences.py
+++ algthk/week7,8 dynamic programming/ComputingAlignmentsOfSequences.py
@@ -29,9 +29,10 @@ def build_scoring_matrix(alphabet, diag_score, off_diag_score, dash_score):
 
 def compute_alignment_matrix(seq_x, seq_y, scoring_matrix, global_flag):
     """
+    这里用来做真正alignment的
     Computes alignment matrix using the method ComputeGlobalAlignmentScores
     
-    Input:seq_x, seq_y are two sequences; scoring_matrix; global_flag
+    Input:Two sequences: seq_x, seq_y; scoring_matrix; global_flag
     (global or local alignment)
     
     Output:An alignment matrix
@@ -59,8 +60,86 @@ def compute_alignment_matrix(seq_x, seq_y, scoring_matrix, global_flag):
     
     return alignment_matrix
 
-def compute_global_alignment():
-    pass
+def compute_global_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
+    """
+    只是trace back的过程
+    Fuction use method ComputeAlignment
+    
+    Input:Two sequences: seq_x, seq_y; scoring_matrix; alignment_matrix
+    
+    Output:tuple(score,align_x,align_y),a global alignment of seq_x and seq_y and its score
+    """
+    len_x, len_y = len(seq_x), len(seq_y)
+    align_x, align_y = "", ""
+    while len_x != 0 and len_y != 0:
+        if alignment_matrix[len_x][len_y] == alignment_matrix[len_x-1][len_y-1] + scoring_matrix[seq_x[len_x-1]][seq_y[len_y-1]]:
+            align_x = seq_x[len_x-1] + align_x
+            align_y = seq_y[len_y-1] + align_y
+            len_x -= 1
+            len_y -= 1
+        else:
+            if alignment_matrix[len_x][len_y] == alignment_matrix[len_x-1][len_y] + scoring_matrix[seq_x[len_x-1]]["-"]:
+                align_x = seq_x[len_x-1] + align_x
+                align_y = "-" + align_y
+                len_x -= 1
+            else:
+                align_x = "-" + align_x
+                align_y = seq_y[len_y-1] + align_y
+                len_y -= 1
+    while len_x != 0:
+        align_x = seq_x[len_x-1] + align_x
+        align_y = "-" + align_y
+        len_x -= 1
+    while len_y != 0:
+        align_x = "-" + align_x
+        align_y = seq_y[len_y-1] + align_y
+        len_y -= 1        
 
-def compute_local_alignment():
-    pass
+    return tuple([alignment_matrix[-1][-1], align_x, align_y])
+
+def compute_local_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
+    """
+    只是trace back的过程
+    compute an optimal local alignment
+    
+    Input:Two sequences: seq_x, seq_y; scoring_matrix; alignment_matrix
+    
+    Output:tuple(score,align_x,align_y),a global alignment of seq_x and seq_y and its score
+    """
+    max_score = -1
+    row = -1
+    col = -1
+    for idx1 in range(len(seq_x)+1):
+        for idx2 in range(len(seq_y)+1):
+            if max_score < alignment_matrix[idx1][idx2]:
+                max_score = alignment_matrix[idx1][idx2]
+                row = idx1
+                col = idx2
+    
+    len_x, len_y = row, col
+    align_x, align_y = "", ""
+    while len_x != 0 and len_y != 0 and alignment_matrix[len_x][len_y] != 0:
+        if alignment_matrix[len_x][len_y] == alignment_matrix[len_x-1][len_y-1] + scoring_matrix[seq_x[len_x-1]][seq_y[len_y-1]]:
+            align_x = seq_x[len_x-1] + align_x
+            align_y = seq_y[len_y-1] + align_y
+            len_x -= 1
+            len_y -= 1
+        else:
+            if alignment_matrix[len_x][len_y] == alignment_matrix[len_x-1][len_y] + scoring_matrix[seq_x[len_x-1]]["-"]:
+                align_x = seq_x[len_x-1] + align_x
+                align_y = "-" + align_y
+                len_x -= 1
+            else:
+                align_x = "-" + align_x
+                align_y = seq_y[len_y-1] + align_y
+                len_y -= 1
+    while len_x != 0 and alignment_matrix[len_x][len_y] != 0:
+        align_x = seq_x[len_x-1] + align_x
+        align_y = "-" + align_y
+        len_x -= 1
+    while len_y != 0 and alignment_matrix[len_x][len_y] != 0:
+        align_x = "-" + align_x
+        align_y = seq_y[len_y-1] + align_y
+        len_y -= 1        
+
+    return tuple([max_score, align_x, align_y])
