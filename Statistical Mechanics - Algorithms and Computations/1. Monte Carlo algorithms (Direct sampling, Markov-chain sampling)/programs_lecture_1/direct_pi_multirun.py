4fb743d6f56718d9143bd02f5645c4b694602ffa
sakimarquis <sakimarquis@qq.com>
2019-09-15 21:29:58 +0800

    starting statistical mechanics

---
+++ Statistical Mechanics - Algorithms and Computations/1. Monte Carlo algorithms (Direct sampling, Markov-chain sampling)/programs_lecture_1/direct_pi_multirun.py
@@ -0,0 +1,19 @@
+import random
+
+def direct_pi(N):
+    n_hits = 0
+    for i in range(N):
+        x, y = random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)
+        if x ** 2 + y ** 2 < 1.0:
+            n_hits += 1
+    return n_hits
+
+n_runs = 1000
+n_trials = 4000
+pi_sims = 0
+for run in range(n_runs):
+    pi_sim = 4.0 * direct_pi(n_trials) / float(n_trials)
+    pi_sims += pi_sim
+    print(pi_sim)
+
+print("average pi = " + str(pi_sims/n_runs))
