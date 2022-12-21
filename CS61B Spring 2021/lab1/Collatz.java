31da527dff83445d3428559cdf7dfade6fc79d12
SakiMarquis <sakimarquis@qq.com>
2022-12-21 10:40:37 -0700

    Update Collatz.java

--- CS61B Spring 2021/lab1/Collatz.java
+++ CS61B Spring 2021/lab1/Collatz.java
@@ -1,17 +1,18 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author hdx
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n % 2 == 1) {
             return 3 * n + 1;
-        } else {
-            return n * 2;
+        } else if (n % 2 == 0) {
+            return n / 2;
         }
+        return 0;
     }
 
     public static void main(String[] args) {
