8fd510997704928d1c0ea55e998a8afc542869c5
SakiMarquis <sakimarquis@qq.com>
2022-12-20 23:06:49 -0700

    Update maxArray.java

--- CS61B Spring 2021/HW0/maxArray.java
+++ CS61B Spring 2021/HW0/maxArray.java
@@ -2,13 +2,18 @@ public class maxArray {
     /** Returns the maximum value from m. */
     public static int max(int[] m) {
         int max_val = -9999;
-        int i = 0;
-        while (i < m.length) {
+        for (int i = 0; i < m.length; i += 1) {
             if (m[i] > max_val) {
                 max_val = m[i];
             }
-            i = i + 1;
         }
+        /** int i = 0;
+         while (i < m.length) {
+            if (m[i] > max_val) {
+                max_val = m[i];
+            }
+            i += 1;
+        }*/
         return max_val;
     }
     public static void main(String[] args) {
