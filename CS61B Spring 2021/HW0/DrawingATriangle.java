7ac46c26c5a53919043830b880eb1d87e46c07ca
SakiMarquis <sakimarquis@qq.com>
2022-12-20 20:15:55 -0700

    Create DrawingATriangle.java

---
+++ CS61B Spring 2021/HW0/DrawingATriangle.java
@@ -0,0 +1,16 @@
+public class DrawingATriangle {
+    public static void main(String[] args) {
+        int row = 0;
+        int SIZE = 5;
+        while (row < SIZE) {
+            int col = 0;
+            while (col <= row) {
+                System.out.print("*");
+                col = col + 1;
+            }
+            System.out.println();
+            row = row + 1;
+        }
+    }
+}
+
