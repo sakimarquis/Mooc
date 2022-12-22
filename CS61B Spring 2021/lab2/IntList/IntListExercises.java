0d08f007ea9384f70bfc3dcb494fa052f62397d9
SakiMarquis <sakimarquis@qq.com>
2022-12-22 14:49:13 -0700

    Update IntListExercises.java

--- CS61B Spring 2021/lab2/IntList/IntListExercises.java
+++ CS61B Spring 2021/lab2/IntList/IntListExercises.java
@@ -26,7 +26,9 @@ public class IntListExercises {
     public static void setToZeroIfMaxFEL(IntList L) {
         IntList p = L;
         while (p != null) {
-            if (firstDigitEqualsLastDigit(max(p))) {
+            int currentMax = max(p);
+            boolean firstEqualsLast = firstDigitEqualsLastDigit(currentMax);
+            if (firstEqualsLast) {
                 p.first = 0;
             }
             p = p.rest;
@@ -51,7 +53,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
