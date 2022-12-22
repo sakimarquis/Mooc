4f4db36c8256c859915c9222a9ad140f313742c2
SakiMarquis <sakimarquis@qq.com>
2022-12-22 10:59:01 -0700

    Update DebugExercise1.java

--- CS61B Spring 2021/lab2/DebugExercise/DebugExercise1.java
+++ CS61B Spring 2021/lab2/DebugExercise/DebugExercise1.java
@@ -5,25 +5,25 @@ package DebugExercise;
  * feature work.
  */
 public class DebugExercise1 {
-    public static int divideThenRound(int top, int bottom) {
-        int quotient = top / bottom;
+    public static int divideThenRound(float top, float bottom) {
+        float quotient = top / bottom;
         int result = Math.round(quotient);
         return result;
     }
 
     public static void main(String[] args) {
-        int t = 10;
-        int b = 2;
+        float t = 10;
+        float b = 2;
         int result = divideThenRound(t, b);
         System.out.println("round(" + t + "/" + b + ")=" + result);
 
-        int t2 = 9;
-        int b2 = 4;
+        float t2 = 9;
+        float b2 = 4;
         int result2 = divideThenRound(t2, b2);
         System.out.println("round(" + t2 + "/" + b2 + ")=" + result2);
 
-        int t3 = 3;
-        int b3 = 4;
+        float t3 = 3;
+        float b3 = 4;
         int result3 = divideThenRound(t3, b3);
         System.out.println("round(" + t3 + "/" + b3 + ")=" + result3);
     }
