3b79d83ee9e9e49ad0c4b03312143fa5e2ca8db9
SakiMarquis <sakimarquis@qq.com>
2022-12-22 15:17:38 -0700

    Update SquarePrimesTest.java

--- CS61B Spring 2021/lab2/IntList/SquarePrimesTest.java
+++ CS61B Spring 2021/lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,22 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesSimple1() {
+        IntList lst = IntList.of(1, 2, 3, 4, 5);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 9 -> 4 -> 25", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesSimple2() {
+        IntList lst = IntList.of(13, 15, 16, 17, 18, 19);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("169 -> 15 -> 16 -> 289 -> 18 -> 361", lst.toString());
+        assertTrue(changed);
+    }
+
+
 }
