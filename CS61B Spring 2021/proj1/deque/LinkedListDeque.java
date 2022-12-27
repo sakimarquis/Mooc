13aa6ca906800a40ebe25f7af63115864e5a713a
SakiMarquis <sakimarquis@qq.com>
2022-12-26 19:48:42 -0700

    Create LinkedListDeque.java

---
+++ CS61B Spring 2021/proj1/deque/LinkedListDeque.java
@@ -0,0 +1,89 @@
+package deque;
+
+/** The generic linked list. */
+public class LinkedListDeque<T> {
+    private class Node {
+        public T item;
+        public Node prev;
+        public Node next;
+
+        public Node(Node p, T i, Node n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    private final Node sentinel;
+    private int size;
+    
+    public LinkedListDeque() {
+        sentinel = new Node(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+
+    public LinkedListDeque(T item) {
+        sentinel = new Node(null, null, null);
+        sentinel.prev = new Node(sentinel, item, sentinel);
+        sentinel.next = new Node(sentinel, item, sentinel);
+        size = 1;
+    }
+
+    public void addFirst(T item) {
+        Node oldFirst = sentinel.next;
+        Node newNode = new Node(sentinel, item, oldFirst);
+        oldFirst.prev = newNode;
+        sentinel.next = newNode;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        Node oldLast = sentinel.prev;
+        Node newNode = new Node(oldLast, item, sentinel);
+        oldLast.next = newNode;
+        sentinel.prev = newNode;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        Node start = sentinel.next;
+        while (start != sentinel) {
+            System.out.print(start.item + " ");
+            start = start.next;
+        }
+        System.out.println();
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        T item = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size -= 1;
+        return item;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        T item = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size -= 1;
+        return item;
+    }
+
+}
