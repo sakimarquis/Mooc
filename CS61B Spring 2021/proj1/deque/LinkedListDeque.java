e4e4670051b5da20a28e6f4723115feb7f7ba1bb
SakiMarquis <sakimarquis@qq.com>
2022-12-26 20:54:20 -0700

    Update LinkedListDeque.java

--- CS61B Spring 2021/proj1/deque/LinkedListDeque.java
+++ CS61B Spring 2021/proj1/deque/LinkedListDeque.java
@@ -113,4 +113,25 @@ public class LinkedListDeque<T> {
             return getRecursiveHelper(index - 1, p.next);
         }
     }
+
+    public boolean equals(Object o) {
+        if (o instanceof LinkedListDeque) {
+            return equalsHelper((LinkedListDeque) o);
+        } else {
+            return false;
+        }
+    }
+
+    private boolean equalsHelper(LinkedListDeque o) {
+        if (this.size() != o.size()) {
+            return false;
+        } else {
+            for (int i = 0; i < this.size(); i++) {
+                if (this.get(i) != o.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
