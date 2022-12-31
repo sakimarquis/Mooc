05b2759c6496d0b7db695758435665a7c99d5c40
SakiMarquis <sakimarquis@qq.com>
2022-12-30 23:46:18 -0700

    Create MaxArrayDeque.java

---
+++ CS61B Spring 2021/proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,25 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private Comparator<T> arrayComparator;
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        Comparator<T> arrayComparator = c;
+    }
+
+    public T max() {
+        if (isEmpty()) {
+            return null;
+        }
+        T max = get(0);
+        for (int i = 1; i < size(); i++) {
+            T item = get(i);
+            if (arrayComparator.compare(item, max) > 0) {
+                max = item;
+            }
+        }
+        return max;
+    }
+}
