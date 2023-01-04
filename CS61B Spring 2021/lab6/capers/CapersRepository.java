2165427d7e10b74a966b884ef55188160cb54b29
SakiMarquis <sakimarquis@qq.com>
2023-01-03 18:13:14 -0700

    set up the FOLDER to store the persistent data

--- CS61B Spring 2021/lab6/capers/CapersRepository.java
+++ CS61B Spring 2021/lab6/capers/CapersRepository.java
@@ -4,22 +4,20 @@ import java.io.File;
 import static capers.Utils.*;
 
 /** A repository for Capers 
- * @author TODO
+ * @author hdx
  * The structure of a Capers Repository is as follows:
  *
  * .capers/ -- top level folder for all persistent data in your lab12 folder
  *    - dogs/ -- folder containing all of the persistent data for dogs
  *    - story -- file containing the current story
  *
- * TODO: change the above structure if you do something different.
  */
 public class CapersRepository {
     /** Current Working Directory. */
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = join(CWD, ".capers");
 
     /**
      * Does required filesystem operations to allow for persistence.
--- CS61B Spring 2021/lab6/capers/Dog.java
+++ CS61B Spring 2021/lab6/capers/Dog.java
@@ -5,14 +5,12 @@ import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author hdx
 */
 public class Dog { // TODO
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
-
+    static final File DOG_FOLDER = join(".capers", "dogs");
     /** Age of dog. */
     private int age;
     /** Breed of dog. */
